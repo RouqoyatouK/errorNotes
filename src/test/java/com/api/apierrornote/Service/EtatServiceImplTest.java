f9c2c425e7baedd95e0b619304aa35ab172ef375
bddiakite <ffffffffff>
2022-09-02 10:52:48 +0000

    commit avant

--- src/test/java/com/api/apierrornote/Service/EtatServiceImplTest.java
+++ src/test/java/com/api/apierrornote/Service/EtatServiceImplTest.java
@@ -1,7 +1,30 @@
 package com.api.apierrornote.Service;
 
+import com.api.apierrornote.Modele.Etat;
+import com.api.apierrornote.Repository.EtatRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class EtatServiceImplTest {
 
+    @Autowired
+    EtatRepo erepo;
+
+    @Autowired
+    EtatService etatService;
+
+    @Test
+    void creer() {
+        Etat etat = new Etat(5L,"Tiec");
+        etatService.creer(etat);
+
+    }
+
+    @Test
+    void trouverParLibelle() {
+    }
 }
--- src/test/java/com/api/apierrornote/Service/UserServiceImplTest.java
+++ src/test/java/com/api/apierrornote/Service/UserServiceImplTest.java
@@ -1,10 +1,52 @@
 package com.api.apierrornote.Service;
 
+import com.api.apierrornote.Modele.User;
+import com.api.apierrornote.Repository.UserRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserServiceImplTest {
 
+    @Autowired
+    UserRepo ur;
+    @Autowired
+    UserService userService;
+
+
+    @Test
+    void creer() {
+        User us = new User();
+        us.setNom("Yng");
+        us.setPrenom("Bk");
+        us.setEmail("bki@gmail.com");
+        us.setPassword("123A");
+        us.setDate(new Date());
+        us.setRole("user");
+        userService.creer(us);
+
+        //User usSave = ur.save(us);
+        /*assertEquals(us.getIdUser(), us.getIdUser());
+        assertEquals(us.getNom(), usSave.getNom());
+        assertEquals(us.getPrenom(), usSave.getPrenom());
+        assertEquals(us.getEmail(), usSave.getEmail());
+        assertEquals(us.getPassword(), usSave.getPassword());
+        assertEquals(us.getDate(), us.getDate());
+        assertEquals(us.getRole(), us.getRole());*/
+
+
+    }
+
+    @Test
+    void supprimer() {
+    }
+
+    @Test
+    void trouverParEmail() {
+    }
 }
